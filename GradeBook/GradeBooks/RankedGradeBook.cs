485bcdd943e7ddc68d00223231c50a53104b16b1
manubassi2009 <45438102+manubassi2009@users.noreply.github.com>
2018-12-09 09:03:58 +0000

    Update RankedGradeBook.cs
    update

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -38,7 +38,7 @@ namespace GradeBook.GradeBooks
             }
             else
             {
-                return base.GetLetterGrade(averageGrade);
+                return 'F';
             }
         }
     }
